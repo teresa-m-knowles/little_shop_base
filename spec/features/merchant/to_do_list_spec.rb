9e64daafe92c2a0d5dd05229b5f0f14a2b41d286
Teresa Knowles <teresa07moreno@gmail.com>
2019-02-22 22:16:33 -0700

    adds test for unfulfilled orders missed revenue

--- spec/features/merchant/to_do_list_spec.rb
+++ spec/features/merchant/to_do_list_spec.rb
@@ -4,9 +4,18 @@ RSpec.describe 'As a merchant', type: :feature do
   context 'In the dashboard, there/s a todo list' do
     before :each do
       @merchant = create(:merchant)
-      @item_1 = create(:item, name: "Item 1", user: @merchant, image: "https://vignette.wikia.nocookie.net/zeldaocarinaoftime/images/b/bf/Bomb_%28Ocarina_of_Time%29.png/revision/latest/scale-to-width-down/180?cb=20120826153735")
-      @item_2 = create(:item, name: "Item 2", user: @merchant)
-      @item_3 = create(:item, name: "Item 3", user: @merchant)
+      @item_1 = create(:item, name: "Item 1", price: 50, inventory: 10, user: @merchant, image: "https://vignette.wikia.nocookie.net/zeldaocarinaoftime/images/b/bf/Bomb_%28Ocarina_of_Time%29.png/revision/latest/scale-to-width-down/180?cb=20120826153735")
+      @item_2 = create(:item, name: "Item 2", price: 20, inventory: 10, user: @merchant)
+      @item_3 = create(:item, name: "Item 3", price: 70, inventory: 10, user: @merchant)
+
+      @customer = create(:user)
+      @pending_order_1 = create(:order, user: @customer)
+      @pending_order_2 = create(:order, user: @customer)
+      @pending_order_3 = create(:order, user: @customer)
+      create(:order_item, order: @pending_order_1, item: @item_1, quantity: 5, price: 50)
+      create(:order_item, order: @pending_order_2, item: @item_2, quantity: 5, price: 20)
+      create(:order_item, order: @pending_order_3, item: @item_3, quantity: 5, price: 70)
+
     end
     describe 'if I have items with placeholder images' do
       it 'shows all items that need an image' do
@@ -38,7 +47,16 @@ RSpec.describe 'As a merchant', type: :feature do
       end
     end
     describe 'if I have unfulfilled items' do
-      it 'I see how many unfulfilled items I have and their revenue' do
+      it 'I see how many unfulfilled items I have and their potential revenue' do
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
+        visit dashboard_path(@merchant)
+
+        within("#to-do-list") do
+          within "#missed-revenue" do
+            expect(page).to have_content("You have 3 unfulfilled orders worth $700")
+            expect(page).to have_content(@item_3.name)
+          end
+        end
       end
     end
     describe 'if I have pending orders that my stock cannot cover' do
