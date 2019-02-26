f95aba58fd1abe45e09039faeb74d47d057e4ec2
Teresa Knowles <teresa07moreno@gmail.com>
2019-02-25 22:14:09 -0700

    adds more tests and delete functionality

---
+++ spec/models/discount_validator_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe DiscountValidator do
+  context 'Validates discounts when they are created' do
+    describe 'when the discount type matches all other discounts types' do
+      it 'can create a new discount' do
+        merchant = create(:merchant)
+        discount_1 = merchant.discounts.create(discount_type: 0, quantity_for_discount: 5, discount_amount: 10)
+        discount_2 = merchant.discounts.create(discount_type: 0, quantity_for_discount: 5, discount_amount: 10)
+
+        expect(discount_2.errors.messages).to eq({})
+      end
+    end
+
+    describe 'when the discount type does not match all other discounts types' do
+      it 'cannot create a new discount' do
+        merchant = create(:merchant)
+        discount_1 = merchant.discounts.create(discount_type: 0, quantity_for_discount: 5, discount_amount: 10)
+        discount_2 = merchant.discounts.create(discount_type: 1, quantity_for_discount: 5, discount_amount: 10)
+
+        expect(discount_2.errors.messages.values.flatten).to include("All of your discounts need to be of the same type.")
+      end
+    end
+  end
+end
