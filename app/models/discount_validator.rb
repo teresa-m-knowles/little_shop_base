90791c1eaba0dafe5f0508d4a1395da8e5822033
Teresa Knowles <teresa07moreno@gmail.com>
2019-02-25 22:35:45 -0700

    adds more meaningful message to user when they try to create a discount with a different type. It asks them to first delete all of their discounts

--- app/models/discount_validator.rb
+++ app/models/discount_validator.rb
@@ -3,7 +3,8 @@ class DiscountValidator < ActiveModel::Validator
     saved_discount = Discount.first
     if Discount.all.count >= 1
       unless discount.discount_type == saved_discount.discount_type
-        discount.errors[:discount_type] << "All of your discounts need to be of the same type."
+        discount.errors[:discount_type] << "All of your discounts need to be of the same type.
+        If you wish to change the type of your discounts, please delete all of your discounts first."
       end
     end
 
