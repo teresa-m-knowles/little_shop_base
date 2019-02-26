e61d110a76f58cbaa995f470f20a0690aafc0164
Teresa Knowles <teresa07moreno@gmail.com>
2019-02-26 02:32:13 -0700

    fixes custom validator class to allow for different merchants to have different discount types

--- app/models/discount_validator.rb
+++ app/models/discount_validator.rb
@@ -1,10 +1,10 @@
 class DiscountValidator < ActiveModel::Validator
   def validate(discount)
-    saved_discount = Discount.where(user: discount.user).limit(1)
-    if Discount.where(user_id: discount.user.id).count >= 1
-      unless discount.discount_type == saved_discount.discount_type
-        discount.errors[:discount_type] << "All of your discounts need to be of the same type.
-        If you wish to change the type of your discounts, please delete all of your discounts first."
+    existing_discount = Discount.find_by(user_id: discount.user_id)
+
+    if existing_discount
+      unless existing_discount.discount_type == discount.discount_type
+        discount.errors[:discount_type] << "All of your discounts need to be of the same type. If you wish to change the type of your discounts, please delete all of your discounts first."
       end
     end
 
